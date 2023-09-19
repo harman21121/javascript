1fb19130f091a670cb63079df17c27d8d4c5b283
Himanshu kumar <132635803+harman21121@users.noreply.github.com>
2023-09-19 11:17:52 +0000

    basics

--- basixs/text.js
+++ basixs/text.js
@@ -1,1 +1,11 @@
-console.log('himanshu kumar')
+// console.log('himanshu kumar');
+
+// let nam= "hello";
+// console.log(typeof nam)
+
+// prompt("hello guys")
+let score = "10";
+number = Number(score)
+console.log(typeof number)
+console.log(number)
+
